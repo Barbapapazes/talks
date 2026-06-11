18c8b49a93b36df10911bf94b122b2c69ad8bcca
Estéban <e.soubiran25@gmail.com>
2026-06-11 11:36:29 +0200

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -10,10 +10,22 @@ export default antfu({
     './**/src/.vite-transformed/**/*',
     './**/src/.vite-build/**/*',
   ],
-}).append({
-  files: ['**/src/slides.md'],
-  rules: {
-    'markdown/no-multiple-h1': 'off',
-    'markdown/require-alt-text': 'off',
-  },
 })
+  .override('antfu/pnpm/pnpm-workspace-yaml', {
+    rules: {
+      'pnpm/yaml-enforce-settings': ['error', {
+        settings: {
+          shellEmulator: true,
+          trustPolicy: 'off',
+          catalogMode: 'prefer',
+        },
+      }],
+    },
+  })
+  .append({
+    files: ['**/src/slides.md'],
+    rules: {
+      'markdown/no-multiple-h1': 'off',
+      'markdown/require-alt-text': 'off',
+    },
+  })
